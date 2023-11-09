37b2a41e280b0137b30729daec35074fc53a034b
JarrettGilp <115848572+JarrettGilp@users.noreply.github.com>
2023-11-09 11:59:59 -0700

    Added Login and Register page support.

--- EffortLoggerV2/src/application/Main.java
+++ EffortLoggerV2/src/application/Main.java
@@ -1,22 +1,46 @@
-// EffortLogger Team Th54
+/*
+ *  EffortLogger V2 - Team Th54
+ *
+ *  This file edited by: Jarrett Gilpatric
+ *
+ */ 
 
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			// SET ROOT PAGE AND CREATE SCENE
+			Parent root = FXMLLoader.load(getClass().getResource("/EffortLoggerLogin.fxml"));
+			Scene homeScene = new Scene(root);
+						
+			// ICON
+			//Image icon = new Image("<something>.png");
+			//stage.getIcons().add(icon);
+						
+			// WINDOW TITLE
+			stage.setTitle("EffortLogger");
+						
+			// WINDOW SIZING
+			stage.setX(450);
+			stage.setY(225);
+						
+			// DISPLAY SCENE
+			stage.setScene(homeScene);
+			stage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- EffortLoggerV2/src/module-info.java
+++ EffortLoggerV2/src/module-info.java
@@ -1,5 +1,6 @@
 module EffortLoggerV2 {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
